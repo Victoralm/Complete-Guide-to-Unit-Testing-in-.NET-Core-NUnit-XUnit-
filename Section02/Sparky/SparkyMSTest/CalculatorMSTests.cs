e077ddfd4710b92dab6714b1b60238dcb054796b
victoralm <victoralmlinx@gmail.com>
2021-09-27 16:14:24 -0300

    S02L12: First Unit Test

---
+++ Section02/Sparky/SparkyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            // Arrange (Test initialization)
+            Calculator calc = new Calculator();
+
+            // Act (Invoking needed methods)
+            int result = calc.AddNumbers(10, 20);
+
+            // Assert (Checking the results)
+            Assert.AreEqual(30, result);
+        }
+    }
+}
