e5911de124824e78622e07cb1d4599e54e09c67d
victoralm <victoralmlinx@gmail.com>
2021-09-30 17:33:49 -0300

    S04L43: Integration test demo

---
+++ Section02/Sparky/SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    [TestFixture]
+    internal class BankAccountNUnitTests
+    {
+        private BankAccount _bankAccount;
+        [SetUp]
+        public void Setup()
+        {
+            this._bankAccount = new(new LogBook());
+        }
+
+        [Test]
+        public void Deposit_Add100_ReturnTrue()
+        {
+            var result = this._bankAccount.Deposit(100);
+            Assert.IsTrue(result);
+            Assert.That(this._bankAccount.GetBalance, Is.EqualTo(100));
+        }
+
+    }
+}
