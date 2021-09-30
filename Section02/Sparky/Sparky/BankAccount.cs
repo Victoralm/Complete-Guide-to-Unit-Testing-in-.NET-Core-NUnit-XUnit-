a74c1615d1bc1eda7010ae6bd301c94826489adf
victoralm <victoralmlinx@gmail.com>
2021-09-30 16:50:16 -0300

    S04L42: Setup Logger Class

---
+++ Section02/Sparky/Sparky/BankAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    public class BankAccount
+    {
+        private int _balance;
+        private readonly ILogBook _logBook;
+
+        // Injecting ILogBook as a dependency
+        public BankAccount(ILogBook logBook)
+        {
+            this._logBook = logBook;
+            this._balance = 0;
+        }
+
+        public bool Deposit(int amount)
+        {
+            this._logBook.Message("Deposit invoked");
+            this._balance += amount;
+            return true;
+        }
+        
+        public bool Withdraw(int amount)
+        {
+            if(amount <= this._balance)
+            {
+                this._logBook.Message("Withdraw invoked");
+                this._balance -= amount;
+                return true;
+            }
+
+            return false;
+        }
+
+        public int GetBalance()
+        {
+            return this._balance;
+        }
+    }
+}
