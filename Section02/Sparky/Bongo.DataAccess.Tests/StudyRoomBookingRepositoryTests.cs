76eca012838cd4fcd55627e54b272ee09e9aef4c
victoralm <victoralmlinx@gmail.com>
2021-10-03 14:08:04 -0300

    S07L75: Data Access Unit Test

--- Section02/Sparky/Bongo.DataAccess.Tests/StudyRoomBookingRepositoryTests.cs
+++ Section02/Sparky/Bongo.DataAccess.Tests/StudyRoomBookingRepositoryTests.cs
@@ -3,6 +3,7 @@ using Bongo.Models.Model;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -63,6 +64,47 @@ namespace Bongo.DataAccess
                 Assert.AreEqual(_studyRoomBooking_One.Date, bookingFromDb.Date);
                 Assert.AreEqual(_studyRoomBooking_One.Email, bookingFromDb.Email);
                 Assert.AreEqual(_studyRoomBooking_One.StudyRoomId, bookingFromDb.StudyRoomId);
+            }
+        }
+
+        [Test]
+        public void GetAllBooking_BookingOneAndTwo_CheckBothTheBookeingFromDatabase()
+        {
+            // arrange
+            var expectedResult = new List<StudyRoomBooking> { this._studyRoomBooking_One, this._studyRoomBooking_Two };
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "temp_Bongo").Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var repository = new StudyRoomBookingRepository(context);
+                repository.Book(this._studyRoomBooking_One);
+                repository.Book(this._studyRoomBooking_Two);
+            }
+
+            // act
+            List<StudyRoomBooking> actualList;
+            using (var context = new ApplicationDbContext(options))
+            {
+                var repository = new StudyRoomBookingRepository(context);
+                actualList = repository.GetAll(null).ToList();
+            }
+
+            // assert
+            CollectionAssert.AreEqual(expectedResult, actualList, new BookingCompare());
+        }
+
+        private class BookingCompare : IComparer
+        {
+            public int Compare(object x, object y)
+            {
+                var booking1 = (StudyRoomBooking)x;
+                var booking2 = (StudyRoomBooking)y;
+
+                if (booking1.BookingId != booking2.BookingId)
+                    return 1;
+                else
+                    return 0;
             }
         }
     }
