f8ce0c1014add62240a6c12ed7a6a8e886c92a2a
victoralm <victoralmlinx@gmail.com>
2021-10-03 02:25:10 -0300

    S07L71: Bongo Models DateChecker p1

---
+++ Section02/Sparky/Bongo.Models.Tests/DateFutureAttributeTests.cs
@@ -0,0 +1,27 @@
+﻿using Bongo.Models.ModelValidations;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bongo.Models
+{
+    [TestFixture]
+    public class DateFutureAttributeTests
+    {
+        [Test]
+        [TestCase(ExpectedResult = true)]
+        public bool DateValidator_InputExpectedDateRange_DateValidity()
+        {
+            DateInFutureAttribute dateInFutureAttribute = new(() => DateTime.Now);
+
+            var result = dateInFutureAttribute.IsValid(DateTime.Now.AddSeconds(100));
+            // To Fail the test
+            //var result = dateInFutureAttribute.IsValid(DateTime.Now.AddSeconds(-100));
+
+            return result;
+        }
+    }
+}
