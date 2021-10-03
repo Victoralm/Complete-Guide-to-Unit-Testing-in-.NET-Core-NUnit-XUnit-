44b8eb24d364e3ea9677bb39d1afa45a80241d37
victoralm <victoralmlinx@gmail.com>
2021-10-03 19:45:05 -0300

    S07L85: Bongo Web Index Action Method Unit Test

---
+++ Section07/Bongo.Web.Test/RoomBookingControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Bongo.Core.Services.IServices;
+using Bongo.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bongo.Web
+{
+    [TestFixture]
+    public class RoomBookingControllerTests
+    {
+        private Mock<IStudyRoomBookingService> _studyRoomBookingService;
+        private RoomBookingController _bookingController;
+
+        [SetUp]
+        public void Setup()
+        {
+            this._studyRoomBookingService = new Mock<IStudyRoomBookingService>();
+            this._bookingController = new RoomBookingController(this._studyRoomBookingService.Object);
+        }
+
+        [Test]
+        public void IndexPage_CallRequest_VerifyGetAllInvoked()
+        {
+            this._bookingController.Index();
+            this._studyRoomBookingService.Verify(x => x.GetAllBooking(), Times.Once);
+        }
+    }
+}
