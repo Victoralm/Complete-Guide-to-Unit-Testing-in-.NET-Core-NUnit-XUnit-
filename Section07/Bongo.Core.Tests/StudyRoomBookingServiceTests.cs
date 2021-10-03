df109176c0a60c1f64dde46f4b9c75d8589b5066
victoralm <victoralmlinx@gmail.com>
2021-10-03 15:26:48 -0300

    S07L78: Bongo Core MOQ Setup and GetAllBooking test

--- Section07/Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
+++ Section07/Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using Bongo.Core.Services;
+using Bongo.DataAccess.Repository.IRepository;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +13,26 @@ namespace Bongo.Core
     [TestFixture]
     public class StudyRoomBookingServiceTests
     {
+        private Mock<IStudyRoomBookingRepository> _studyRoomBookingRepoMock;
+        private Mock<IStudyRoomRepository> _studyRoomRepoMock;
+        private StudyRoomBookingService _bookingService;
 
+        [SetUp]
+        public void Setup()
+        {
+            this._studyRoomBookingRepoMock = new Mock<IStudyRoomBookingRepository>();
+            this._studyRoomRepoMock = new Mock<IStudyRoomRepository>();
+            this._bookingService = new StudyRoomBookingService(
+                this._studyRoomBookingRepoMock.Object,
+                this._studyRoomRepoMock.Object
+                );
+        }
+
+        [Test]
+        public void GelAllBooking_InvokeMethod_CheckIfRepoIsCalled()
+        {
+            this._bookingService.GetAllBooking();
+            this._studyRoomBookingRepoMock.Verify(x => x.GetAll(null), Times.Once);
+        }
     }
 }
